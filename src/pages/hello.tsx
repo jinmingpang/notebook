168044700491fc67d640ccb362c128eda5a6c28f
careyToboo <toboos@sina.com>
2022-10-11 23:16:13 +0800

    添加游戏相关文档

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+// import OverviewFlow from '../components/flow';
 
 export default function Hello() {
   return (
     <Layout title="Hello" description="Hello React Page">
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}
-      >
-        <p>
-          修改 <code>pages/helloReact.js</code>，然后保存，页面会重载。
-        </p>
+      <div style={{ width: '1000px', height: '600px', margin: 'auto' }}>
+        {/* <OverviewFlow /> */}
       </div>
     </Layout>
   );
